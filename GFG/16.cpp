55c674a42a2f4f55ee815e443eddec99bb396650
arushisharma <39919147+1234arushi@users.noreply.github.com>
2022-06-26 10:48:41 +0530

    Create 16.cpp

---
+++ GFG/16.cpp
@@ -0,0 +1,53 @@
+//https://practice.geeksforgeeks.org/problems/non-repeating-element3958/1
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+ 
+
+ // } Driver Code Ends
+
+class Solution{
+    public:
+    int firstNonRepeating(int arr[], int n) 
+    { 
+        unordered_map<int, int> umap;
+        for(int i=0;i<n;i++)
+        {
+            umap[arr[i]]++;
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(umap[arr[i]]==1)
+            {
+                return arr[i];
+            }
+        }
+        return 0;
+        // Complete the function
+        
+    } 
+  
+};
+
+
+// { Driver Code Starts.
+
+int main()
+{
+
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int arr[n];
+        for (int i=0; i<n; i++)
+        {
+            cin>>arr[i];
+        }
+        Solution ob;
+        cout<<ob.firstNonRepeating(arr,n)<<endl;
+    }
+}
+  // } Driver Code Ends
