f6d26dda9c4b85b1d46bd98be230cec679be63db
arushisharma <39919147+1234arushi@users.noreply.github.com>
2022-07-20 16:00:19 +0530

    Update 14.cpp

--- GFG/14.cpp
+++ GFG/14.cpp
@@ -10,7 +10,46 @@ class Solution{
 public:
 
 	void rearrange(int arr[], int n) {
-	    vector<int> v1;
+	    
+	    //Method 2:
+	    vector<int> p;
+	    vector<int> ne;
+	    for(int i=0;i<n;i++)
+	    {
+	        if(arr[i]>=0)
+	        {
+	            p.push_back(arr[i]);
+	        }
+	        else
+	        {
+	            ne.push_back(arr[i]);
+	        }
+	    }
+	    int i=0,j=0,k=0;
+	    while(i<p.size() && j<ne.size())
+	    {
+	        if(k%2==0)
+	        {
+	            arr[k++]=p[i++];
+	        }
+	        else
+	        {
+	            arr[k++]=ne[j++];
+	        }
+	    }
+	    while(i<p.size())
+	    {
+	        arr[k++]=p[i++];
+	    }
+	     while(j<ne.size())
+	    {
+	        arr[k++]=ne[j++];
+	    }
+	    
+	    
+	    
+	    //Method 1:
+	    /*vector<int> v1;
 	    vector<int> v2;
 	    
 	    for(int i=0;i<n;i++)
@@ -40,5 +79,32 @@ public:
 	                arr[k++]=v2[j++];
 	            }
 	        }
-	    }
+	    }*/
 	    
+	  
+	    // code here
+	}
+};
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n;
+        int arr[n];
+        for (i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        ob.rearrange(arr, n);
+        for (i = 0; i < n; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+  // } Driver Code Ends
