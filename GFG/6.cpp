73bd00acd398769697962fbe15ba1e4f3dc31d8e
arushisharma <39919147+1234arushi@users.noreply.github.com>
2022-10-09 17:02:22 +0530

    Update 6.cpp

--- GFG/6.cpp
+++ GFG/6.cpp
@@ -1,3 +1,73 @@
+//https://leetcode.com/problems/sort-colors/
+
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        //Method 2:
+        int count0=0,count1=0,count2=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            switch(nums[i])
+            {
+                case 0:
+                    count0++;
+                    break;
+                case 1:
+                    count1++;
+                    break;
+                case 2:
+                    count2++;
+                    break;
+            }
+        }
+        int i=0;
+        while(count0>0)
+        {
+            nums[i++]=0;
+            count0--;
+        }
+         while(count1>0)
+        {
+            nums[i++]=1;
+            count1--;
+        }
+        while(count2>0)
+        {
+            nums[i++]=2;
+            count2--;
+        }
+       
+            
+            
+        //Method 1:Dutch national flag algo
+        
+        /*int low=0;
+        int high=nums.size()-1;
+        int mid=0;
+        while(mid<=high)
+        {
+            if(nums[mid]==0)
+            {
+                swap(nums[low],nums[mid]);
+                low++;mid++;
+            }
+            else if(nums[mid]==1)
+            {
+                mid++;
+            }
+            else if(nums[mid]==2)
+            {
+                swap(nums[mid],nums[high]);
+                high--;
+            }
+        }*/
+        
+    }
+};
+
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------
 //https://practice.geeksforgeeks.org/problems/sort-an-array-of-0s-1s-and-2s4231/1
 // { Driver Code Starts
 #include<bits/stdc++.h>
