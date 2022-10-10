e3e21a78cd4b68d4f2814866f7f1f60b78e515b4
arushisharma <39919147+1234arushi@users.noreply.github.com>
2022-10-10 19:03:00 +0530

    Update 22.cpp

--- GFG/22.cpp
+++ GFG/22.cpp
@@ -1,35 +1,50 @@
 //https://practice.geeksforgeeks.org/problems/chocolate-distribution-problem3825/1#
-// { Driver Code Starts
+
+//{ Driver Code Starts
 #include <bits/stdc++.h>
 using namespace std;
 
- // } Driver Code Ends
+// } Driver Code Ends
 class Solution{
     public:
     long long findMinDiff(vector<long long> a, long long n, long long m){
-        
-        //TC: O(n log n)        
-        sort(a.begin(),a.end());//nlog n time 
+    //code
+        //another method:
+        sort(a.begin(),a.end());
         int min_diff=INT_MAX;
-        for(int i=0;i+m-1<n;i++)//n time
+       
+        for(int i=0;i+m-1<n;i++)//i+m-1<n---->jb tk tumahra 'm' size ka subarray
+                               //milra tb tk loop work krega
         {
             int diff=a[i+m-1]-a[i];
             if(diff<min_diff)
             {
                 min_diff=diff;
-                
             }
-            //mindiff=min(mindiff,a[i+m-1]-a[i]);why min() didnt work
-            
+        } 
+        return min_diff;
+                              
+        //my method:
+        /*sort(a.begin(),a.end());
+        long long diff=a[m-1]-a[0];
+        
+        int j=1;
+        for(int i=m;i<n;i++)
+        {
+            long long x=a[j];
+           
+            long long y=a[i];
+           
+            j++;
+            diff=min(diff,y-x);
             
         }
-        return min_diff;
-    //code
-    
+        
+        return diff;*/
     }   
 };
 
-// { Driver Code Starts.
+//{ Driver Code Starts.
 int main() {
 	long long t;
 	cin>>t;
@@ -51,4 +66,5 @@ int main() {
 		cout<<ob.findMinDiff(a,n,m)<<endl;
 	}
 	return 0;
-}  // } Driver Code Ends
+}
+// } Driver Code Ends
