463a89dd32e1115bc3ac18f6eac45b29256b79c2
arushisharma <39919147+1234arushi@users.noreply.github.com>
2022-10-13 11:16:40 +0530

    Update 25.cpp

--- GFG/25.cpp
+++ GFG/25.cpp
@@ -3,54 +3,24 @@ class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         
-        //why sorting:get more detailed explanation
-        sort(intervals.begin(),intervals.end());//how 2D vector sorted
-        //maybe according to first element if not specified?
-       //sort:O(n log n)
+        vector<vector<int>> merged;
+        sort(intervals.begin(),intervals.end());//default sort from start time
+        //[[1,4],[0,4]]--->i.e sorting is needed
         
-        vector <vector<int>> merged;
-        for(auto interval:intervals)//linear scan of list?-->O(n)
+        for(auto interval:intervals)
         {
-            //what does merged.back()[1] and interval[0] implies which element..confirm
-            if(merged.empty()||merged.back()[1]<interval[0])
+            if(merged.empty()|| merged.back()[1]<interval[0])
             {
                 merged.push_back(interval);
             }
             else
             {
-                merged.back()[1]=max(merged.back()[1],interval[1]);//max necessary?
+                //why we take max :[[1,4],[2,3]]
+                merged.back()[1]=max(merged.back()[1],interval[1]);
             }
         }
-        //TC:O(n log n)
-       // SC:O(log n)-->sorting.Otherwise, we must allocate linear space to
-        //store a copy of intervals and sort that.
-        
         return merged;
         
-        
-        /*my approach:runtime error
-        vector <vector<int>> v;
-        
-        for(int i=0;i<intervals.size()-1;i++)
-        {
-            for(int j=0;j<intervals[i].size()-1;j++)
-            {
-                if(intervals[i][j]<intervals[i+1][j]<intervals[i][j+1]) 
-                {
-                    v[i][j]=intervals[i][j];
-                    v[i][j+1]=intervals[i+1][j+1];
-                  
-                }
-                else
-                {
-                    v[i][j]=intervals[i][j];
-                    v[i][j+1]=intervals[i][j+1];
-                    
-                }
-                
-            }
-        }
-        return v;*/
         
     }
 };
