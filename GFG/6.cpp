48942205c8ed21883cd9ee960b7ddc3ac3f323a1
arushisharma <39919147+1234arushi@users.noreply.github.com>
2022-11-17 10:31:50 +0530

    Update 6.cpp

--- GFG/6.cpp
+++ GFG/6.cpp
@@ -1,9 +1,9 @@
 //https://leetcode.com/problems/sort-colors/
 
-
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
+
         //Method 2:
         int count0=0,count1=0,count2=0;
         for(int i=0;i<nums.size();i++)
@@ -11,61 +11,54 @@ public:
             switch(nums[i])
             {
                 case 0:
-                    count0++;
-                    break;
+                   count0++;
+                   break;
                 case 1:
-                    count1++;
-                    break;
+                   count1++;
+                   break;
                 case 2:
                     count2++;
                     break;
+
             }
+            
+           
         }
         int i=0;
-        while(count0>0)
+        while(count0--)
         {
             nums[i++]=0;
-            count0--;
         }
-         while(count1>0)
+        while(count1--)
         {
             nums[i++]=1;
-            count1--;
         }
-        while(count2>0)
+        while(count2--)
         {
             nums[i++]=2;
-            count2--;
         }
-       
-            
-            
-        //Method 1:Dutch national flag algo
-        
-        /*int low=0;
-        int high=nums.size()-1;
-        int mid=0;
+
+        //Method 1:using dutch national flag algo
+        /*int low=0,mid=0,high=nums.size()-1;
         while(mid<=high)
         {
-            if(nums[mid]==0)
-            {
-                swap(nums[low],nums[mid]);
-                low++;mid++;
-            }
-            else if(nums[mid]==1)
+            if(nums[mid]==1)
             {
                 mid++;
             }
+            else if(nums[mid]==0)
+            {
+                swap(nums[low++],nums[mid++]);
+
+            }
             else if(nums[mid]==2)
             {
-                swap(nums[mid],nums[high]);
-                high--;
+                swap(nums[high--],nums[mid]);
             }
         }*/
         
     }
 };
-
 
 --------------------------------------------------------------------------------------------------------------------------------------------------------------------
 //https://practice.geeksforgeeks.org/problems/sort-an-array-of-0s-1s-and-2s4231/1
